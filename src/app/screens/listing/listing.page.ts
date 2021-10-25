dc6a55e34652c4f0e7782fc3a84c61099ed0c511
kwalsh15 <kwalsh@pernixlabs.com>
2021-10-24 21:55:52 -0600

    add initial data

--- src/app/screens/listing/listing.page.ts
+++ src/app/screens/listing/listing.page.ts
@@ -24,6 +24,7 @@ export class ListingPage {
   }
 
   async ionViewWillEnter() {
+    this.loadInitialData();
     this.foods = [];
     this.foods = await this.recipeService.index();
   }
@@ -34,5 +35,67 @@ export class ListingPage {
 
   comments(id: number){
     this.router.navigate(['comments', id]);
+  }
+
+  async loadInitialData() {
+    const recipes = await this.recipeService.index();
+    console.log(recipes);
+    if(recipes.length == 0) {
+      let recipe: Recipe = {
+        "id": 1,
+        "name": "Molotes de Oaxaca",
+        "author": "Juanito Martínez",
+        "imageUrl": "https://i.blogs.es/6d3c0a/1366_2000-4/840_560.jpg",
+        "description": "Esta receta es deliciosa",
+        "comments": [],
+        "steps": [
+          {
+            "name": "",
+            "ingredients": ["chili", "flour"],
+            "cookware": ["spoon", "knife"],
+            "videoUrl": "https://www.youtube.com/embed/KXw8CRapg7k"
+          }
+        ]
+      }
+      this.recipeService.save(recipe).then(res => {
+        const recipe1 = {
+          "id": 2,
+          "name": "Arroz Con Leche",
+          "author": "Manuel Amores",
+          "imageUrl": "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F21%2F2018%2F03%2F25%2Frecetas-1092-arroz-con-leche-2000.jpg&q=85",
+          "description": "Esta receta es deliciosa",
+          "comments": [],
+          "steps": [
+            {
+              "name": "name",
+              "ingredients": ["rice", "milk", "cinnamon"],
+              "cookware": ["spoon", "knife"],
+              "videoUrl": "https://www.youtube.com/watch?v=xQsPiAf-t1Q"
+            }
+          ]
+        }
+        this.recipeService.save(recipe1).then(res => {
+          const recipe2 = {
+            "id": 3,
+            "name": "Arroz Con Atún",
+            "author": "Manuel Quesada",
+            "imageUrl": "https://i.ytimg.com/vi/27f3_Ja0JcY/maxresdefault.jpg",
+            "description": "Esta receta es deliciosa",
+            "comments": [],
+            "steps": [
+              {
+                "name": "LMAO",
+                "ingredients": ["rice", "tuna"],
+                "cookware": ["spoon", "knife"],
+                "videoUrl": "https://www.youtube.com/watch?v=27f3_Ja0JcY"
+              }
+            ]
+          }
+    
+          this.recipeService.save(recipe2);
+    
+        });
+      });
+    }
   }
 }
